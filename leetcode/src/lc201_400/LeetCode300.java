18c1d6260f4ab503fb62de279965b7582be1692f
zhualaoshudemao <752658686@qq.com>
2019-08-17 11:10:48 +0800

    [add] 300

--- leetcode/src/lc201_400/LeetCode300.java
+++ leetcode/src/lc201_400/LeetCode300.java
@@ -1,4 +1,13 @@
-class Solution {
+package lc201_400;
+
+/**
+ * 最长上升子序列
+ * https://leetcode-cn.com/problems/longest-increasing-subsequence/submissions/
+ *
+ * @author binzhang
+ * @date 2019-08-17
+ */
+public class LeetCode300 {
     public int lengthOfLIS(int[] nums) {
         if (nums.length <= 1) {
             return nums.length;
